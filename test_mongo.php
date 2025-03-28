0ab0f6fed96f5f39b2ea8c2724b55b2de816cc7e
Hoang Thien <hoangthien@ip-192-168-1-3.ap-southeast-1.compute.internal>
2025-03-28 19:58:16 +0700

    chore: add MongoDB connection test script

---
+++ test_mongo.php
@@ -0,0 +1,15 @@
+<?php
+
+$uri = "mongodb+srv://danghoangthien:Q6b4Mo8cddDbQViz@challege-vsee-cluster.qun9h9b.mongodb.net/vsee?retryWrites=true&w=majority&appName=challege-vsee-cluster";
+
+try {
+    $manager = new MongoDB\Driver\Manager($uri);
+    echo "Connected successfully\n";
+    
+    $command = new MongoDB\Driver\Command(['ping' => 1]);
+    $manager->executeCommand('vsee', $command);
+    echo "Database ping successful\n";
+} catch (Exception $e) {
+    echo "Error: " . $e->getMessage() . "\n";
+    echo "Trace: " . $e->getTraceAsString() . "\n";
+} 

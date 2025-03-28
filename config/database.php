40397a6f00dbf05cbf4afbc0bf960a195ffe13e9
Hoang Thien <hoangthien@ip-192-168-1-3.ap-southeast-1.compute.internal>
2025-03-28 18:01:19 +0700

    Create necessary Laravel storage directories

--- config/database.php
+++ config/database.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Str;
 
+$mongoUrl = parse_url(env('MONGODB_URI', ''));
+
 return [
 
     /*
@@ -16,7 +18,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'sqlite'),
+    'default' => env('DB_CONNECTION', 'mysql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -44,15 +46,15 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => env('DB_URL'),
+            'url' => env('JAWSDB_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'laravel'),
-            'username' => env('DB_USERNAME', 'root'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
-            'charset' => env('DB_CHARSET', 'utf8mb4'),
-            'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
             'prefix_indexes' => true,
             'strict' => true,
@@ -114,8 +116,14 @@ return [
 
         'mongodb' => [
             'driver' => 'mongodb',
-            'dsn' => env('MONGODB_URI', 'mongodb://localhost:27017/waiting_room'),
-            'database' => env('MONGODB_DATABASE', 'waiting_room'),
+            'host' => $mongoUrl['host'] ?? env('MONGODB_HOST', '127.0.0.1'),
+            'port' => $mongoUrl['port'] ?? env('MONGODB_PORT', 27017),
+            'database' => ltrim($mongoUrl['path'] ?? env('MONGODB_DATABASE'), '/'),
+            'username' => $mongoUrl['user'] ?? env('MONGODB_USERNAME'),
+            'password' => $mongoUrl['pass'] ?? env('MONGODB_PASSWORD'),
+            'options' => [
+                'authSource' => env('MONGODB_AUTH_DATABASE', 'admin'),
+            ],
         ],
 
     ],
@@ -149,12 +157,11 @@ return [
 
     'redis' => [
 
-        'client' => env('REDIS_CLIENT', 'phpredis'),
+        'client' => env('REDIS_CLIENT', 'predis'),
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
             'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
-            'persistent' => env('REDIS_PERSISTENT', false),
         ],
 
         'default' => [
